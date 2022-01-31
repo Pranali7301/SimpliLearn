681a1036924b82dd45de42667ffcdce67eb72804
Pranali7301 <97670028+Pranali7301@users.noreply.github.com>
2022-01-31 18:44:13 +0530

    Add files via upload

---
+++ Phase2/AssistedProject2/HelloWorld/src/Calculate.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Calculate {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Enter two numbers:");
+		
+		int firstNumber = scan.nextInt();
+		int secondNumber = scan.nextInt(); 
+		
+		int result = firstNumber + secondNumber;
+		
+		System.out.println("Addition of numbers is : " + result);
+		
+	}
+
+}
+
